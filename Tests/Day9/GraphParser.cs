01812d476537a59d213f9629ab6bb2fcda98de18
Łukasz Brodny <brodny@wp.pl>
2016-05-20 20:12:22 +0200

    Day 9 initial implementation

---
+++ Tests/Day9/GraphParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Tests.Day9
+{
+    public class GraphParser : IGraphParser
+    {
+        public IGraph Parse(string[] graphDescription)
+        {
+            if (graphDescription == null)
+                throw new ArgumentNullException(nameof(graphDescription));
+            if (graphDescription.Any(descriptionLine => string.IsNullOrWhiteSpace(descriptionLine)))
+                throw new ArgumentException("One of descriptions is null or empty", nameof(graphDescription));
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/Day9/ShortestDistanceCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tests.Day9
+{
+    public sealed class ShortestDistanceCalculator : IShortestDistanceCalculator, IGraphDistanceCalculator
+    {
+        public int Calculate(IGraph graph)
+        {
+            if (graph == null)
+                throw new ArgumentNullException(nameof(graph));
+
+            throw new NotImplementedException();
+        }
+    }
+}
