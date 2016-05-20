e6147b3517d4fdf2d56527442ffa61dbafba9d87
Łukasz Brodny <brodny@wp.pl>
2016-05-20 20:47:32 +0200

    Day 9 implementation starts

--- Tests/Day9/Graph.cs
+++ Tests/Day9/Graph.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Tests.Day9
 {
     public sealed class Graph : IGraph
     {
+        public bool ContainsVertex(string vertex)
+        {
+            CheckVertexName(vertex);
+            return _vertices.Contains(vertex);
+        }
+
+        public void AddVertex(string vertex)
+        {
+            CheckVertexName(vertex);
+            if (ContainsVertex(vertex))
+                throw new ArgumentException($"Vertex '{vertex}' already exists.", nameof(vertex));
+            _vertices.Add(vertex);
+        }
+
+        public void AddEdge(string firstVertex, string secondVertex, int weight)
+        {
+            CheckVertexName(firstVertex);
+            CheckVertexName(secondVertex);
+            CheckIfVertexExists(firstVertex);
+            CheckIfVertexExists(secondVertex);
+
+            Tuple<string, string> edge = Tuple.Create(firstVertex, secondVertex);
+            _edges.Add(edge, weight);
+        }
+
+        private void CheckVertexName(string vertex)
+        {
+            if (!VertexNameIsCorrect(vertex))
+                throw new ArgumentNullException(nameof(vertex));
+        }
+
+        private bool VertexNameIsCorrect(string vertex)
+        {
+            return !string.IsNullOrWhiteSpace(vertex);
+        }
+
+        private void CheckIfVertexExists(string vertex)
+        {
+            Debug.Assert(VertexNameIsCorrect(vertex));
+            if (!ContainsVertex(vertex))
+                throw new ArgumentException($"Vertex '{vertex}' does not exist", nameof(vertex));
+        }
+
+        private readonly HashSet<string> _vertices = new HashSet<string>();
+        private readonly Dictionary<Tuple<string, string>, int> _edges = new Dictionary<Tuple<string, string>, int>();
     }
 }
--- Tests/Day9/GraphParser.cs
+++ Tests/Day9/GraphParser.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Tests.Day9
 {
@@ -12,7 +15,54 @@ namespace Tests.Day9
             if (graphDescription.Any(descriptionLine => string.IsNullOrWhiteSpace(descriptionLine)))
                 throw new ArgumentException("One of descriptions is null or empty", nameof(graphDescription));
 
-            throw new NotImplementedException();
+            Graph graph = new Graph();
+
+            for (int i = 0; i < graphDescription.Length; ++i)
+            {
+                ParseDescription(graphDescription[i], graph);
+            }
+
+            return graph;
         }
+
+        private void ParseDescription(string graphDescription, Graph graph)
+        {
+            Debug.Assert(!string.IsNullOrWhiteSpace(graphDescription));
+            Debug.Assert(graph != null);
+
+            GroupCollection matches = _parserRegex.Match(graphDescription).Groups;
+            if (matches.Count != 4)
+                throw new ArgumentException("Invalid input", nameof(graphDescription));
+
+            string firstCity = matches[1].Value;
+            string secondCity = matches[2].Value;
+            string distanceStr = matches[3].Value;
+
+            if (string.IsNullOrWhiteSpace(firstCity))
+                throw new ArgumentException("First city is not specified", nameof(graphDescription));
+            if (string.IsNullOrWhiteSpace(secondCity))
+                throw new ArgumentException("Second city is not specified", nameof(graphDescription));
+            if (string.IsNullOrWhiteSpace(distanceStr))
+                throw new ArgumentException("Distance is not specified", nameof(graphDescription));
+
+            int distance;
+            if (!int.TryParse(distanceStr, NumberStyles.Integer, CultureInfo.InvariantCulture, out distance))
+                throw new ArgumentException("Distance is not a number", nameof(graphDescription));
+
+            AddVertexIfDoesNotExist(graph, firstCity);
+            AddVertexIfDoesNotExist(graph, secondCity);
+            graph.AddEdge(firstCity, secondCity, distance);
+        }
+
+        private static void AddVertexIfDoesNotExist(Graph graph, string vertex)
+        {
+            Debug.Assert(graph != null);
+            Debug.Assert(!string.IsNullOrWhiteSpace(vertex));
+
+            if (!graph.ContainsVertex(vertex))
+                graph.AddVertex(vertex);
+        }
+
+        private readonly Regex _parserRegex = new Regex(@"^(\w+)\sto\s(\w+)\s=\s(\d+)$", RegexOptions.Compiled);
     }
 }
