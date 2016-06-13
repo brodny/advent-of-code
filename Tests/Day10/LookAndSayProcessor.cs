a91292ff82254f81de87394c19abcf01a4936057
Łukasz Brodny <brodny@wp.pl>
2016-06-13 19:05:58 +0200

    Day 10 test cases solved

--- Tests/Day10/LookAndSayProcessor.cs
+++ Tests/Day10/LookAndSayProcessor.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests.Day10
 {
@@ -10,7 +8,40 @@ namespace Tests.Day10
     {
         public string Process(string input)
         {
-            throw new NotImplementedException();
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            StringBuilder resultBuilder = new StringBuilder();
+
+            int i = 0;
+            while (i < input.Length)
+            {
+                string sameCharacters = GetTheSameCharacters(input, i);
+                Debug.Assert(sameCharacters != null);
+                Debug.Assert(sameCharacters.Length > 0);
+
+                resultBuilder.Append(sameCharacters.Length).Append(sameCharacters[0]);
+                i += sameCharacters.Length;
+            }
+
+            return resultBuilder.ToString();
+        }
+
+        private string GetTheSameCharacters(string input, int startingIndex)
+        {
+            Debug.Assert(input != null);
+            Debug.Assert(startingIndex >= 0);
+            Debug.Assert(startingIndex < input.Length);
+
+            int sameCharactersEndIndex = startingIndex + 1;
+            while (sameCharactersEndIndex < input.Length && input[sameCharactersEndIndex] == input[startingIndex])
+            {
+                ++sameCharactersEndIndex;
+            }
+
+            int sameCharactersSubstringLength = sameCharactersEndIndex - startingIndex;
+            string sameCharactersString = input.Substring(startingIndex, sameCharactersSubstringLength);
+            return sameCharactersString;
         }
     }
 }
