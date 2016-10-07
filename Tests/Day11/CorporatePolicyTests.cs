ea13b5bd860e158db30c83b60001e4282af5a4b1
Łukasz Brodny <brodny@wp.pl>
2016-10-07 19:00:18 +0200

    Add Day11 structure

---
+++ Tests/Day11/CorporatePolicyTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Day11
+{
+    [TestFixture]
+    public sealed class CorporatePolicyTests : TestBase
+    {
+    }
+}
